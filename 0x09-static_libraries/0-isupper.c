f9eaa53e8342d05eea68d1a686f5d39786b2c40a
Ifyobi <chibuzor.md@gmail.com>
2022-10-05 12:40:34 -0700

    libmy.a

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checks uppercase character
+ *
+ * @c: integer of uppercase character
+ * Return: 1 if c and zero if otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
