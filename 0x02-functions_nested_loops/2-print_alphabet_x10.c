d7eea682dbb2cee2a2be9ccaec47760a9f806f5d
Ifyobi <chibuzor.md@gmail.com>
2022-10-05 10:26:55 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet_x10 in lowercase
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char j;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
